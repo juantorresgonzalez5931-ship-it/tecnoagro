df687feab764c55eb89c579683907d94ea99f36f
LuisValencia0 <cadenvalencia320@gmail.com>
2026-08-03 11:07:49 -0500

    cambiando

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,1 +1,11 @@
+import bycrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import {crearUsuario, obtenerPorEmail} from '../models/usuario.js';
 
+
+// registro
+export const registro = async (req, res) => {
+    const {nombre, email, password} = req.body;
+
+    //validar datos
+}
